612757f699fa68002f7122827d0e7bf2097aee38
LongTH <tranlong.kma@gmail.com>
2020-12-02 10:47:24 +0700

    fix config app

--- lib/configuration_app/configuration_app.dart
+++ lib/configuration_app/configuration_app.dart
@@ -1,39 +1,25 @@
 import 'package:flutter/material.dart';
 
-abstract class AppBase extends StatelessWidget {
-  AppBase({Key key}) : super(key: key ?? GlobalKey());
-}
-
 class AppConfig extends InheritedWidget {
   AppConfig({
     @required this.appName,
     @required this.flavorName,
     @required this.apiUrl,
-    this.apiUsername,
-    this.apiPassword,
+    @required this.secret,
+    @required this.clientId,
     this.wss,
-    this.oneSignalID,
     @required Widget child,
-  }) : super(child: Material(child: child)) {
+  }) : super(child: child) {
     _globalKey = child.key;
   }
 
-  ///name of app
   final String appName;
-
-  ///environment development (main/dev/testing)
   final AppFlavor flavorName;
-
-  ///url of api
   final String apiUrl;
-
-  ///wss(web socket server). use with graphql
+  final String secret;
   final String wss;
+  final int clientId;
 
-  ///use for connect with id one signal to push notify
-  final String oneSignalID;
-  final String apiUsername;
-  final String apiPassword;
   static final GlobalKey<NavigatorState> navigatorKey =
       GlobalKey<NavigatorState>();
   static GlobalKey _globalKey;
@@ -42,8 +28,6 @@ class AppConfig extends InheritedWidget {
 
   static AppConfig get instance => _globalKey.currentContext
       .dependOnInheritedWidgetOfExactType(aspect: AppConfig);
-
-  bool get isDevelopment => flavorName == AppFlavor.DEVELOPMENT;
 
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) => false;
